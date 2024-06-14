363516a462669083620222f80bbebe8b723cccff
prajwal-jagadeesh <156173951+prajwal-jagadeesh@users.noreply.github.com>
2024-06-14 12:22:54 +0000

    Day 01 File added

---
+++ DAY01/Day01.py
@@ -0,0 +1,8 @@
+name1 = "prajwal"
+name2 = "kiran"
+result = name1 + " " + name2
+print(result.upper())
+length = len(result)
+print("Length is :", length)
+name3 = "prajwal kiran"
+print(name3.split(" ",-2))
